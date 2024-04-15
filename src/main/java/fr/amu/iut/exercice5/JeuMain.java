e7883d7526f7443a1ff26cf42f746fc96cd0ff27
Leo Garcia <leo.garcia.5@etu.univ-amu.fr>
2024-04-15 12:16:26 +0200

    Exercice 5

--- src/main/java/fr/amu/iut/exercice5/JeuMain.java
+++ src/main/java/fr/amu/iut/exercice5/JeuMain.java
@@ -1,7 +1,9 @@
 package fr.amu.iut.exercice5;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -12,10 +14,24 @@ public class JeuMain extends Application {
     private Scene scene;
     private BorderPane root;
 
+    private Obstacle obstacle;
+
+
+    private double posXj1;
+    private double posYj1;
+    private double posXj2;
+    private double posYj2;
+
     @Override
     public void start(Stage primaryStage) {
 
         root = new BorderPane();
+
+        //Obstacles
+        obstacle = new Obstacle( 300,200,100,300);
+
+
+
 
         //Acteurs du jeu
         Personnage pacman = new Pacman();
@@ -27,6 +43,7 @@ public class JeuMain extends Application {
         jeu.setPrefSize(640, 480);
         jeu.getChildren().add(pacman);
         jeu.getChildren().add(fantome);
+        jeu.getChildren().add(obstacle);
         root.setCenter(jeu);
         //on construit une scene 640 * 480 pixels
         scene = new Scene(root);
@@ -56,13 +73,43 @@ public class JeuMain extends Application {
                 case RIGHT:
                     j1.deplacerADroite(scene.getWidth());
                     break;
+                case UP:
+                    j1.deplacerEnHaut();
+                    break;
+                case DOWN:
+                    j1.deplacerEnBas(scene.getHeight());
+                    break;
+                case Q:
+                    j2.deplacerAGauche();
+                    break;
+                case D:
+                    j2.deplacerADroite(scene.getWidth());
+                    break;
                 case Z:
-                    //j2...... vers le haut;
+                    j2.deplacerEnHaut();
+                    break;
+                case S:
+                    j2.deplacerEnBas(scene.getHeight());
                     break;
 
             }
-            if (j1.estEnCollision(j2))
+            if (j1.estEnCollision(j2)) {
                 System.out.println("Collision....");
+                Platform.exit();
+            } else if (obstacle.estEnCollision(j1)) {
+                System.out.println("Collision....");
+                j1.setLayoutX(posXj1);
+                j1.setLayoutY(posYj1);
+            } else if (obstacle.estEnCollision(j2)) {
+                System.out.println("Collision....");
+                j2.setLayoutX(posXj2);
+                j2.setLayoutY(posYj2);
+            } else {
+                posXj1 = j1.getLayoutX();
+                posYj1 = j1.getLayoutY();
+                posXj2 = j2.getLayoutX();
+                posYj2 = j2.getLayoutY();
+            }
         });
     }
 
