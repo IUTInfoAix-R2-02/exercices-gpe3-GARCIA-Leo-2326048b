aac0f5b26d2a1fb3f8f987c723df03d047897b5b
Leo Garcia <leo.garcia.5@etu.univ-amu.fr>
2024-04-15 09:39:33 +0200

    Exercice 4

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,8 +1,12 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -18,13 +22,49 @@ public class Palette extends Application {
     private Button rouge;
     private Button bleu;
 
-    private BorderPane root;
     private Label label;
     private Pane panneau;
-    private HBox bas;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root,400,200);
+        primaryStage.setScene(scene);
+
+        label = new Label();
+        BorderPane.setAlignment(label,Pos.CENTER);
+
+        panneau = new Pane();
+
+        HBox bas = new HBox();
+        bas.setAlignment(Pos.CENTER);
+        bas.setPadding(new Insets(10));
+        vert = new Button("vert");
+        rouge = new Button("rouge");
+        bleu = new Button("bleu");
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbVert++;
+            label.setText(vert.getText()+" choisi "+nbVert+" fois");
+            panneau.setStyle("-fx-background-color: green");
+        });
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbRouge++;
+            label.setText(rouge.getText()+" choisi "+nbRouge+" fois");
+            panneau.setStyle("-fx-background-color: red");
+        });
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbBleu++;
+            label.setText(bleu.getText()+" choisi "+nbBleu+" fois");
+            panneau.setStyle("-fx-background-color: blue");
+        });
+        bas.setSpacing(10);
+        bas.getChildren().addAll(vert,rouge,bleu);
+
+        root.setTop(label);
+        root.setCenter(panneau);
+        root.setBottom(bas);
+
         primaryStage.show();
     }
 }
